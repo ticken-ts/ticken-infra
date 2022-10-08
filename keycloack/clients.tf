721cd4f690330c88761f6d36bd18dec9d700587e
facundotorraca <facundo@innovid.com>
2022-10-08 14:19:44 -0300

    add aud mapper on attendants app

--- keycloack/clients.tf
+++ keycloack/clients.tf
@@ -1,13 +1,43 @@
-resource "keycloak_openid_client" "postman_attendant_app" {
-  realm_id            = keycloak_realm.attendants_realm.id
-  client_id           = "postman-attendant-app"
+resource "keycloak_openid_client_scope" "attendant_app" {
+  realm_id               = keycloak_realm.attendants_realm.id
+  name                   = "attendant-app"
+  description            = "add all necessary scopes to attendant's app"
+}
 
-  name                = "postman attendant app"
-  enabled             = true
+resource "keycloak_openid_audience_protocol_mapper" "attendant_audience_mapper" {
+  realm_id                 = keycloak_realm.attendants_realm.id
+  client_scope_id          = keycloak_openid_client_scope.attendant_app.id
+  name                     = "ticken-attendants-app-audience-mapper"
+  included_custom_audience = "ticken-attendants-app"
+}
+
+resource "keycloak_openid_client" "postman_attendant_app" {
+  realm_id                     = keycloak_realm.attendants_realm.id
+  client_id                    = "postman-attendant-app"
+         
+  name                         = "postman attendant app"
+  enabled                      = true
 
   access_type                  = "PUBLIC"
   direct_access_grants_enabled = true
 }
+
+resource "keycloak_openid_client_default_scopes" "postman_attendant_app_default_scopes" {
+  realm_id  = keycloak_realm.attendants_realm.id
+  client_id = keycloak_openid_client.postman_attendant_app.id
+
+  default_scopes = [
+    "profile",
+    "email",
+    "roles",
+    "web-origins",
+    keycloak_openid_client_scope.attendant_app.name,
+  ]
+}
+
+
+
+
 
 resource "keycloak_openid_client" "postman_organizer_app" {
   realm_id            = keycloak_realm.organizers_realm.id
