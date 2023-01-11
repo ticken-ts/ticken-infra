8a45da3352f4e3cf589d9cbbc2135c50fd1b6743
facundotorraca <facundo@innovid.com>
2023-01-11 02:45:34 -0300

    add gossip endpoint as peer environment variable to solve gateway discovery error

--- pvtbc/k8s-artifacts/scripts/channel/org-join-channel.sh
+++ pvtbc/k8s-artifacts/scripts/channel/org-join-channel.sh
@@ -1,10 +1,8 @@
 . /scripts/utils/org-context.sh
 
-function generate_org_definition() {
+function _generate_org_definition() {
   local org_name=$1
   local storage_path=$2
-
-  echo "generation org ${org_name} definition and storing in ${storage_path}/configtx.yaml"
 
   local org_msp="${org_name}MSP"
 
@@ -38,13 +36,11 @@ function generate_org_definition() {
   FABRIC_CFG_PATH=${storage_path} configtxgen -printOrg ${org_msp} > ${storage_path}/${org_name}.json
 }
 
-function fetch_channel_config() {
+function _fetch_channel_config() {
   local channel=$1
   local genesis_org=$2
   local orderer=$3
   local config_filename=$4
-
-  echo "fetching ${channel} config using ${genesis_org} and storing in ${config_filename}"
 
   set_peer_org_context $genesis_org
 
@@ -63,9 +59,9 @@ function fetch_channel_config() {
   jq .data.data[0].payload.data.config config_block.json > "${config_filename}"
 }
 
-function add_org_to_channel_config() {
+function _add_org_to_channel_config() {
   local org=$1
-  local org_defintion=$2
+  local org_definition=$2
   local current_channel_config=$3
   local output_modified_config=$4
 
@@ -73,11 +69,26 @@ function add_org_to_channel_config() {
 
   jq -s \
     ".[0] * {\"channel_group\":{\"groups\":{\"Application\":{\"groups\": {\"${org_msp}\":.[1]}}}}}" \
-    ${current_channel_config} ${org_defintion} > ${output_modified_config}
+    ${current_channel_config} ${org_definition} > ${output_modified_config}
+}
+
+function _add_anchor_peer_to_channel_config() {
+  local org=$1
+  local node=$2
+  local current_channel_config=$3
+  local output_modified_config=$4
+
+  local peer_port=7051
+  local peer_host="${org}-${node}"
+
+  local org_msp="${org}MSP"
+
+  jq '.channel_group.groups.Application.groups.'${org_msp}'.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "'${peer_host}'","port": '${peer_port}'}]},"version": "0"}}' \
+  ${current_channel_config} > ${output_modified_config}
 }
 
 
-function create_channel_config_update() {
+function _create_channel_config_update() {
   CHANNEL=$1
   ORIGINAL=$2
   MODIFIED=$3
@@ -89,18 +100,15 @@ function create_channel_config_update() {
   configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
   echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . >config_update_in_envelope.json
   configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output "${OUTPUT}"
-  { set +x; } 2>/dev/null
 }
 
-function submit_channel_update_transaction() {
+function _submit_channel_update_transaction() {
   local channel=$1
   local orderer=$2
-  local genesis_org=$3
+  local submiter_org=$3
   local update_envelope=$4
 
-  echo "submiting ${channel} update using ${genesis_org}"
-
-  set_peer_org_context $genesis_org
+  set_peer_org_context $submiter_org
 
   local orderer_ca_file="/orgs/orderer-orgs/${orderer}/nodes/${orderer}-orderer0/tls/signcerts/tls-cert.pem"
 
@@ -111,22 +119,59 @@ function submit_channel_update_transaction() {
     --tls --cafile ${orderer_ca_file}
 }
 
+function add_org_to_channel() {
+  local org_name=$1
+  local channel=$2
+  local genesis_org_name=$3
+  local orderer_org_name=$4
 
-CONFIGTX_PATH="/configtx"
-mkdir -p $CONFIGTX_PATH
+  CONFIGTX_PATH="/configtx"
+  mkdir -p $CONFIGTX_PATH
+
+  ORG_DEFINITION_FILE="${CONFIGTX_PATH}/${org_name}.json"
+  ORIGINAL_CHANNEL_CONFIG="channel-config-current.json"
+  MODIFIED_CHANNEL_CONFIG="channel-config-modified.json"
+  UPDATE_ENVELOPE_OUTPUT="update-channel-envelope.pb"
+
+  _generate_org_definition $org_name $CONFIGTX_PATH
+  _fetch_channel_config $channel $genesis_org_name $orderer_org_name $ORIGINAL_CHANNEL_CONFIG
+  _add_org_to_channel_config $org_name $ORG_DEFINITION_FILE $ORIGINAL_CHANNEL_CONFIG $MODIFIED_CHANNEL_CONFIG
+  _create_channel_config_update $channel $ORIGINAL_CHANNEL_CONFIG $MODIFIED_CHANNEL_CONFIG $UPDATE_ENVELOPE_OUTPUT
+  _submit_channel_update_transaction $channel $orderer_org_name $genesis_org_name $UPDATE_ENVELOPE_OUTPUT
+
+  rm $ORIGINAL_CHANNEL_CONFIG
+  rm $MODIFIED_CHANNEL_CONFIG
+  rm $UPDATE_ENVELOPE_OUTPUT
+}
+
+function set_org_anchor_peer() {
+  set -x
+  local org_name=$1
+  local channel=$2
+  local genesis_org_name=$3
+  local orderer_org_name=$4
+
+  ORIGINAL_CHANNEL_CONFIG="channel-config-current.json"
+  MODIFIED_CHANNEL_CONFIG="channel-config-modified.json"
+  UPDATE_ENVELOPE_OUTPUT="update-channel-envelope.pb"
+
+  _fetch_channel_config $channel $genesis_org_name $orderer_org_name $ORIGINAL_CHANNEL_CONFIG
+  _add_anchor_peer_to_channel_config $org_name "peer0" $ORIGINAL_CHANNEL_CONFIG $MODIFIED_CHANNEL_CONFIG
+
+  cat $MODIFIED_CHANNEL_CONFIG
+
+  _create_channel_config_update $channel $ORIGINAL_CHANNEL_CONFIG $MODIFIED_CHANNEL_CONFIG $UPDATE_ENVELOPE_OUTPUT
+  _submit_channel_update_transaction $channel $orderer_org_name $org_name $UPDATE_ENVELOPE_OUTPUT
+
+  rm $ORIGINAL_CHANNEL_CONFIG
+  rm $MODIFIED_CHANNEL_CONFIG
+  rm $UPDATE_ENVELOPE_OUTPUT
+}
 
 CHANNEL_NAME=$1
 JOINING_ORG_NAME=$2
 GENESIS_ORG_NAME=$3
 ORDERER_ORG_NAME=$4
 
-ORIGINAL_CHANNEL_CONFIG="channel-config-original.json"
-MODIFIED_CHANNEL_CONFIG="channel-config-modified.json"
-
-UPDATE_ENVELOPE_OUTPUT="update-channel-envelope.pb"
-
-generate_org_definition $JOINING_ORG_NAME $CONFIGTX_PATH
-fetch_channel_config $CHANNEL_NAME $GENESIS_ORG_NAME $ORDERER_ORG_NAME $ORIGINAL_CHANNEL_CONFIG
-add_org_to_channel_config $JOINING_ORG_NAME "${CONFIGTX_PATH}/${JOINING_ORG_NAME}.json" $ORIGINAL_CHANNEL_CONFIG $MODIFIED_CHANNEL_CONFIG
-create_channel_config_update $CHANNEL_NAME $ORIGINAL_CHANNEL_CONFIG $MODIFIED_CHANNEL_CONFIG $UPDATE_ENVELOPE_OUTPUT
-submit_channel_update_transaction $CHANNEL_NAME $ORDERER_ORG_NAME $GENESIS_ORG_NAME $UPDATE_ENVELOPE_OUTPUT
+add_org_to_channel $JOINING_ORG_NAME $CHANNEL_NAME $GENESIS_ORG_NAME $ORDERER_ORG_NAME
+set_org_anchor_peer $JOINING_ORG_NAME $CHANNEL_NAME $GENESIS_ORG_NAME $ORDERER_ORG_NAME
